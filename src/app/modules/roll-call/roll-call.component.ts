513516433fa1ed553b1a50a723a85c549a27faa8
vladementei@gmail.com <Pravda1650>
2020-08-23 14:20:47 +0300

    [UI] feat: add rxjs unsubscribe mechanism

--- src/app/modules/roll-call/roll-call.component.ts
+++ src/app/modules/roll-call/roll-call.component.ts
@@ -4,6 +4,8 @@ import {Observable} from "rxjs";
 import {Select, Store} from "@ngxs/store";
 import {SetPresentCountries} from "../../core/state/roll-call/roll-call.actions";
 import {CountryModel} from "../../models/country.model";
+import {RxUnsubscribe} from "../../core/services/rx-unsubscribe";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'roll-call',
@@ -11,16 +13,18 @@ import {CountryModel} from "../../models/country.model";
   styleUrls: ['./roll-call.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RollCallComponent {
+export class RollCallComponent extends RxUnsubscribe {
 
   @Select(RollCallSelectors.presentCountries) presentCountries$: Observable<CountryModel[]>;
 
   constructor(private store: Store) {
+    super();
     //setTimeout(() => this.setCountries(), 5000);
   }
 
   private setCountries() {
     this.store.dispatch(new SetPresentCountries([{name: 'Japan'}, {name: 'Russia'}]))
+      .pipe(takeUntil(this.destroy$))
       .subscribe(() => {
         console.log(this.store.selectSnapshot<CountryModel[]>(state => state.root.rollCall.presentCountries));
       });
